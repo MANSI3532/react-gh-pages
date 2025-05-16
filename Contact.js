31f893581f60b4f1e10d4d92a62b03326bdf3faf
MANSI3532 <138848442+MANSI3532@users.noreply.github.com>
2025-05-16 10:51:17 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="section">
+      <h2>Contact</h2>
+      <p>Email: <a href="mailto:mansi.cm1204@gmail.com">mansi.cm1204@gmail.com</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/mansi-c-m-6a8bb2300/">Mansi C M</a></p>
+    </section>
+  );
+}
+
+export default Contact;
